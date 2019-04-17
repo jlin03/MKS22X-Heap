eddd7bb4a2afcc3126d2284dad52171f853b8e57
jlin03 <jlin03@stuy.edu>
2019-04-17 16:48:21 -0400

    fixed pushDown for uneven trees and odd indexes

--- MyHeap.java
+++ MyHeap.java
@@ -17,32 +17,36 @@ public class MyHeap {
 	}
 	
 	private static void pushDown(int[] data, int size, int index) {
-		while(2*index + 1 < size-1 && (data[index] < data[2*index + 1] || data[index] < data[2*index + 2])) {
-			int next = getMax(data,2*index + 1,2*index + 2);
+		int next;
+		if(2*index + 2 <= size) {
+			next = getMax(data,2*index + 1,2*index + 2);
+		}
+		else { 
+			next = 2*index + 1;
+		}
+		while(next <= size && data[next] > data[index]) {
 			swap(data,index,next);
 			index = next;
-		}
-		if(size == 2) {
-			int next = getMax(data,index,2*index + 1);
-			swap(data,index,next);
+			if(2*index + 2 < size) {
+				next = getMax(data,2*index + 1,2*index + 2);
+			}
+			else { 
+				next = 2*index + 1;
+			}
 		}
 	}
 	
 	public static void heapify(int[] data) {
-		for(int i = data.length-1; i >=0; i--) {
+		for(int i = data.length-1; i >= 0; i--) {
 			pushDown(data,data.length-1,i);
 		}
 	}
 	
 	public static void heapsort(int[] data) {
 		heapify(data);
-		for(int i = data.length; i > 0; i--) {
+		for(int i = data.length-1; i > 0; i--) {
 			pushDown(data,i,0);
-			System.out.println(Arrays.toString(data));
-			swap(data,0,i-1);
-			System.out.println(i);
-			System.out.println(Arrays.toString(data));
-			System.out.println();
+			swap(data,0,i);
 		}
 	}
 	
@@ -51,13 +55,16 @@ public class MyHeap {
 		Random r = new Random(System.currentTimeMillis());
 		int size = Integer.parseInt(args[0]);
 		int[] test = new int[size];
+		int[] compare = new int[size];
 		for(int i = 0; i < test.length; i++) {
 			test[i] = Math.abs(r.nextInt() % 100);
+			compare[i] = test[i];
 		}
 		long x = System.currentTimeMillis();
 		heapsort(test);
-		System.out.println(Arrays.toString(test));
 		System.out.println((System.currentTimeMillis() - x) + " milliseconds");
+		Arrays.sort(compare);
+		System.out.println(Arrays.equals(test,compare));
 		
 	}
 	
