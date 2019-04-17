e9747733ff8f24486c58c210042519fcbc34fe20
jlin03 <jlin03@stuy.edu>
2019-04-17 15:17:30 -0400

    heapify done

--- MyHeap.java
+++ MyHeap.java
@@ -16,7 +16,7 @@ public class MyHeap {
 		data[two] = temp;
 	}
 	
-	private static void pushDown(int[]data, int size, int index) {
+	private static void pushDown(int[] data, int size, int index) {
 		while(2*index + 1 < size && (data[index] < data[2*index + 1] || data[index] < data[2*index + 2])) {
 			int next = getMax(data,2*index + 1,2*index + 2);
 			swap(data,index,next);
@@ -24,13 +24,17 @@ public class MyHeap {
 		}
 	}
 	
-	
+	public static void heapify(int[] data) {
+		for(int i = data.length-1; i >=0; i--) {
+			pushDown(data,data.length-1,i);
+		}
+	}
 	
 	
 	
 	public static void main(String[] args) {
-		int[] test = {1,6,3,2,87,4,2};
-		pushDown(test,test.length,0);
+		int[] test = {1,6,3,2,87,4,2,31,165,16,4,16,14,15,31,1};
+		heapify(test);
 		System.out.println(Arrays.toString(test));
 		
 		
